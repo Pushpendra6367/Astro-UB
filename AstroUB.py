bb6adc8e5739e874cad2f3872b63e59f7e180858
ʟ๏ᴠᴇʀ ◜‿◝ <79841865+loverboyXD@users.noreply.github.com>
2021-08-18 12:17:35 +0530

    <Astro> </UB>

---
+++ AstroUB.py
@@ -0,0 +1,8 @@
+from os import system
+
+system(
+    "git clone https://github.com/loverboyXD/Astro-UserBot && cd Astro-UserBot && python3 -m astro"
+)
+# if got error
+print("You Have Got Error While Deployment Report and Get Help from @Astro_HelpChat")
+# Script written for stop heroku suspension
