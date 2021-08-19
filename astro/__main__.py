97d6c43357036bf6bfb1c28bdf384817c2a4fc19
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-19 14:06:16 +0530

    •×Astro-Setup×•

--- astro/__main__.py
+++ astro/__main__.py
@@ -23,7 +23,7 @@ async def add_bot(bot_token):
 
 async def startup_log_all_done():
     try:
-        await bot.send_message(GROUP, f"Hey..{OWNER_USERNAME} ƛsτʀ๏ IS ON👮\nFor Your security😌\nNow you are 100% Safe By astro-Security😉\nUSE `.alive` To check me😁\n\n Add @{BOT_USERNAME} To get Notifications Related to Astro-UB \n~Enjoy~\n\n~ @Astro_HelpChat")
+        await bot.send_message(GROUP, f"Hey..{OWNER_USERNAME} ƛsτʀ๏ IS ON👮\nFor Your security😌\nNow you are 100% Safe By astro-Security😉\nUSE `.alive` To check me😁\n\n Add {BOT_USERNAME} To get Notifications Related to Astro-UB \n~Enjoy~\n\n~ @Astro_HelpChat")
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
